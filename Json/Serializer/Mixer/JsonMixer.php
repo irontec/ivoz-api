d8a23946d8e15d714c275bc27726e8a293486570
mmadariaga <mikel@irontec.com>
2020-03-13 09:51:28 +0100

    Optimized unpaginated JSON response memory usage

--- Json/Serializer/Mixer/JsonMixer.php
+++ Json/Serializer/Mixer/JsonMixer.php
@@ -3,10 +3,25 @@
 namespace Ivoz\Api\Json\Serializer\Mixer;
 
 use Ivoz\Api\Core\Serializer\Mixer\MixerInterface;
+use Ivoz\Core\Infrastructure\Persistence\Filesystem\TempFileHelper;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class JsonMixer implements MixerInterface
 {
     const FORMAT = 'json';
+
+    protected $requestStack;
+    protected $tempFileHelper;
+
+    public function __construct(
+        RequestStack $requestStack,
+        TempFileHelper $tempFileHelper
+    ) {
+        $this->requestStack = $requestStack;
+        $this->tempFileHelper = $tempFileHelper;
+    }
 
     /**
      * @inheritdoc
@@ -21,20 +36,68 @@ class JsonMixer implements MixerInterface
      */
     public function mix(array $segments)
     {
-        $cleanSegments = array_map(
-            function ($item) {
-                return substr(
-                    stream_get_contents($item),
-                    1,
-                    -1
+        $tmpFile = $this
+            ->tempFileHelper
+            ->createWithContent(
+                '[',
+                false
+            );
+
+        for ($i = 0; $i < count($segments); $i++) {
+
+            $content = $i === 0
+                ? ''
+                : ',';
+
+            $content .= substr(
+                stream_get_contents($segments[$i]),
+                1,
+                -1
+            );
+
+            $this
+                ->tempFileHelper
+                ->appendContent(
+                    $tmpFile,
+                    $content
                 );
-            },
-            $segments
+        }
+
+        $this
+            ->tempFileHelper
+            ->appendContent(
+                $tmpFile,
+                ']'
+            );
+
+        $responseCallback = function () use ($tmpFile) {
+
+            fseek($tmpFile, 0);
+            while (!feof($tmpFile)) {
+                $chunk = fread($tmpFile, 1024);
+                if ($chunk !== false) {
+                    echo $chunk;
+                }
+            }
+            flush();
+        };
+
+        $request = $this
+            ->requestStack
+            ->getCurrentRequest();
+
+        $contentType = sprintf(
+            '%s; charset=utf-8',
+            $request->getMimeType($request->getRequestFormat())
         );
 
-        return
-            '['
-            . implode(',', $cleanSegments)
-            . ']';
+        return new StreamedResponse(
+            $responseCallback,
+            self::METHOD_TO_CODE[$request->getMethod()] ?? Response::HTTP_OK,
+            array_merge(
+                self::BASE_RESPONSE_HEADERS,
+                ['Content-Type' => $contentType]
+            )
+        );
     }
 }
