edf3f06ac6e40c33b6bed73e78475f5136f65de2
Mikel <mikel@irontec.com>
2022-07-26 12:50:53 +0200

    added multidelete support

---
+++ EventListener/ValidateAccessControlTrait.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Ivoz\Api\EventListener;
+
+use Ivoz\Api\Core\Security\DataAccessControlHelper;
+use Ivoz\Api\Core\Security\DataAccessControlParser;
+use Ivoz\Core\Application\DataTransferObjectInterface;
+use Ivoz\Core\Application\Service\Assembler\DtoAssembler;
+use Ivoz\Core\Domain\Model\EntityInterface;
+use Symfony\Component\DependencyInjection\ExpressionLanguage;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+trait ValidateAccessControlTrait
+{
+    /**
+     * @var DataAccessControlParser
+     */
+    private $dataAccessControlParser;
+
+    /**
+     * @var DtoAssembler
+     */
+    private $dtoAssembler;
+
+    /**
+     * @throws AccessDeniedException
+     */
+    protected function validateAccessControlOrThrowException(EntityInterface $entity, string $method)
+    {
+        $dataAccessControlMode = $method === Request::METHOD_DELETE
+            ? DataAccessControlParser::DELETE_ACCESS_CONTROL_ATTRIBUTE
+            : DataAccessControlParser::WRITE_ACCESS_CONTROL_ATTRIBUTE;
+
+        $dataAccessControl = $this->dataAccessControlParser->get(
+            $dataAccessControlMode
+        );
+        $accessControlExpression = DataAccessControlHelper::toString($dataAccessControl);
+        if (!$accessControlExpression) {
+            return;
+        }
+
+        $data = $this
+            ->dtoAssembler
+            ->toDto($entity)
+            ->toArray();
+
+        $accessControlData = $this->flattenAccessControlData($data);
+        $expressionLanguage = new ExpressionLanguage();
+
+        $isValid = $expressionLanguage->evaluate(
+            $accessControlExpression,
+            $accessControlData
+        );
+
+        if (!$isValid) {
+            throw new AccessDeniedException('Rejected request during security check');
+        }
+    }
+
+    protected function flattenAccessControlData(array $data)
+    {
+        foreach ($data as $key => $value) {
+            if ($value instanceof DataTransferObjectInterface) {
+                $data[$key] = $value->getId();
+            }
+        }
+
+        return $data;
+    }
+}
